918f160f9bf8f6020c48d5c86978cf1d9afc0700
Xu Chong <xc540863058@gmail.com>
2016-08-26 09:56:49 +0800

    fixed untracked file

--- target/classes/import.sql
+++
@@ -1,4 +0,0 @@
-insert into record (date, stock, symbol, currency, buyprice, stocksize, networth, comments) values ('May 20', 'Apple', 'AAPL', 'USD', 50.00, 100.00, 5000.00, 'abc');
-insert into record (date, stock, symbol, currency, buyprice, stocksize, networth, comments) values ('June 20', 'Alibaba', 'BABA', 'CNY', 1000.00, 1000.00, 1000000.00, 'def');
-insert into record (date, stock, symbol, currency, buyprice, stocksize, networth, comments) values ('July 20', 'Bayerische Moteren Werke AG', 'BMW', 'EUR', 100.00, 10.00, 1000.00, 'ghi');
-insert into record (date, stock, symbol, currency, buyprice, stocksize, networth, comments) values ('August 20', 'Infosys', 'INFY', 'INR', 1500.00, 1000.00, 1500000.00, 'jkl');
